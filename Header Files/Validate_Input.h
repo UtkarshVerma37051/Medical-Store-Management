c8941a7185c552aea142226cd764a1c6c5c29f55
Utkarsh Verma <utkarsh.verma.37051@gmail.com>
2021-02-15 07:54:54 +0530

    Add files via upload

---
+++ Header Files/Validate_Input.h
@@ -0,0 +1,9 @@
+#include<regex>
+using namespace std;
+#ifndef VALIDATE_INPUT_H_INCLUDED
+#define VALIDATE_INPUT_H_INCLUDED
+float check_price(string num);
+int check_number(string num);
+long long int check_phone_number(string num);
+int check_mail(string& mail);
+#endif // VALIDATE_INPUT_H_INCLUDED
